e77424f8435388f03feeea60f4aa6cb27cbdb6d9
Nycolas Felipe <nycolasfelipe.contato@gmail.com>
2025-06-15 16:47:24 -0300

    fix(hashPassword): Correção para caso em que password é uma string vazia

--- src/util/hashPassword.ts
+++ src/util/hashPassword.ts
@@ -1,6 +1,10 @@
 import crypto from "crypto";
+import ErrorMessage from "src/errors/ErrorMessage";
 
 const hashPassword = (password: string) => {
+  if (!password) {
+    throw new ErrorMessage("o campo senha não pode ser vazio.", 400);
+  }
   return crypto.createHash('sha256').update(password).digest('hex')
 }
 
