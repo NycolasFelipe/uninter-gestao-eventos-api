dd411e7638523c439bda532c71fcc6ac7f54aace
Nycolas Felipe <nycolasfelipe.contato@gmail.com>
2025-06-15 19:31:42 -0300

    fix(AuthRoute): Remoção do middleware de autenticação para rota de login

--- src/routes/AuthRoute.ts
+++ src/routes/AuthRoute.ts
@@ -1,10 +1,9 @@
 import express from "express";
 import AuthController from "src/controllers/AuthController";
-import authenticateJWT from "src/middlewares/authenticateJWT";
 
 const router = express.Router();
 const controller = new AuthController();
 
-router.post("/login", authenticateJWT, controller.login);
+router.post("/login", controller.login);
 
 export default router;
