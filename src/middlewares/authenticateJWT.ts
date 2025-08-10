97dd228ba4e7504438ad038567c1e91172beccdb
Nycolas Felipe <nycolasfelipe.contato@gmail.com>
2025-08-10 16:30:30 -0300

    feat(authenticateJWT): Criação de contexto de requisição, para acessar dados do token de acesso globalmente

--- src/middlewares/authenticateJWT.ts
+++ src/middlewares/authenticateJWT.ts
@@ -1,18 +1,26 @@
 import { Request, Response, NextFunction } from "express";
-import dotenv from 'dotenv';
 import jwt from "jsonwebtoken";
+import dotenv from 'dotenv';
 
-// Carregar variáveis ​​de ambiente
+// Util e erros internos
+import ErrorMessage from "src/errors/ErrorMessage";
+import warnMissingEnv from "src/util/warnMissingEnv";
+import { requestContext } from "src/context/requestContext";
+
+// Carrega as variáveis de ambiente
 dotenv.config();
 
-// Recuperar variáveis ​​de ambiente
-const ADMIN_FIXED_TOKEN = process.env.ADMIN_FIXED_TOKEN;
-const SECRET = process.env.SECRET;
-
-// Validar variáveis ​​de ambiente
-if (!ADMIN_FIXED_TOKEN || !SECRET) {
-  throw new Error("Variáveis de ambiente necessárias não encontradas");
+// Variáveis de ambiente necessárias
+const env = {
+  ADMIN_FIXED_TOKEN: process.env.ADMIN_FIXED_TOKEN!,
+  SECRET: process.env.SECRET!
 }
+
+// Valida as variáveis de ambiente e alerta sobre quaisquer faltantes
+warnMissingEnv(env);
+
+// Desestruturação das variáveis de ambiente após validação
+const { ADMIN_FIXED_TOKEN, SECRET } = env;
 
 const authenticateJWT = (req: Request, res: Response, next: NextFunction): void => {
   // Primeiro verifica o token fixo da equipe admin
@@ -20,39 +28,49 @@ const authenticateJWT = (req: Request, res: Response, next: NextFunction): void 
 
   // Se o token fixo foi enviado e é válido, permite acesso
   if (fixedToken && fixedToken === ADMIN_FIXED_TOKEN) {
-    return next(); // Acesso concedido via token fixo
+    return next();
   }
 
   // Se não usar token fixo, verifica o JWT normal
   const authHeader = req.headers.authorization;
-  const token = authHeader && authHeader.split(" ")[1]; // Extrai o Bearer token
+  const token = authHeader && authHeader.replace(/^Bearer\s+/i, '');
+
+  const handleError = (message: string) => {
+    throw new ErrorMessage(message, 401);
+  }
 
   if (!token) {
-    res.status(401).json({ message: "Token não fornecido ou formato inválido" });
-    return; 
+    return handleError("Token não fornecido ou formato inválido");
   }
 
   // Verifica o token JWT
   jwt.verify(token, SECRET, (error, payload) => {
+    const handleError = (message: string) => {
+      throw new ErrorMessage(message, 401);
+    };
+
     if (error) {
-      if (error.name === "JsonWebTokenError") {
-        res.status(401).json({ message: "Erro na validação do token" });
+      switch (error.name) {
+        case 'JsonWebTokenError':
+          return handleError('Erro na validação do token');
+        case 'TokenExpiredError':
+          return handleError('Token expirado');
+        case 'Invalid token':
+        default:
+          return handleError(error.message === 'Invalid token'
+            ? 'Token inválido'
+            : 'Erro ao verificar token');
       }
-      else if (error.name === "TokenExpiredError") {
-        res.status(401).json({ message: "Token expirado" });
-      }
-      else if (error.message === "Invalid token") {
-        res.status(401).json({ message: "Token inválido" });
-      }
-      return;
     }
 
-    // Injeta dados do payload na request
+    // Injeta dados do usuário na requisição e contexto
     req.payload = payload as jwt.JwtPayload;
 
-    // Acesso concedido via JWT
-    next();
+    requestContext.run(
+      { payload: req.payload },
+      async () => next()
+    );
   });
-};
+}
 
 export default authenticateJWT;
